f6515f2d09856609ff03a10c53d1c2601d4d4b96
cjredmond <connor.redmond@gmail.com>
2016-10-25 13:59:33 -0400

    initial commit

---
+++ todomvc/todo/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from todo.models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('__all__')
+
+
+class TodoFinisher(serializers.ModelSerializer):
+    class Meta:
+        def get_completed(self):
+            return getattr(completed = True)
+        model = Todo
+        fields = ('__all__')
